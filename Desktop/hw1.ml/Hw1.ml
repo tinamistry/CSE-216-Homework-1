d71e5c0cb0bf53b39dab0ca07dd99e1acd329a0d
Tina Mistry <tmistry10@gmail.com>
2021-09-29 18:52:01 -0400

    Adding questions 1-3

--- Desktop/hw1.ml/Hw1.ml
+++ Desktop/hw1.ml/Hw1.ml
@@ -1,16 +1,39 @@
 let rec pow x n = 
   if n == 0 then 
   1
-  else if n== 1 then 
-  x
-  else 
-  x * pow x (n-1);;
-   
+else if n == 1 then 
+ x
+ else 
+ x * pow x (n-1);;
+
 
 let rec float_pow x n = 
-    if n == 0 
-    1.0
-    else if n == 1 then 
-    x
-    else
-    x *. pow x (n - 1);;
+   if n == 0 then 
+   1.0
+   else if n == 1 then 
+   x 
+   else 
+   x *. float_pow x (n-1);;
+
+  
+   let rec compress lst  = 
+    match lst with 
+    |[] -> []
+    |[head] -> [head]
+    |head :: (head_of_tail) -> if head = head_of_tail then (compress(head_of_tail::tail)) else head::(compress(head_of_tail));;
+
+   
+let rec remove_if list p = 
+  match list with 
+  |[] -> []
+  |head::tail -> if p head = true then remove_if tail p else head::(remove_if tail p);;
+  
+
+
+
+  
+
+
+
+
+   
