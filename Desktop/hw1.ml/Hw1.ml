ae11d7e3656f711636fb9829299139cc715f1cca
Tina Mistry <tmistry10@gmail.com>
2021-10-01 14:28:29 -0400

    Adding questions 1.6 and 1.8

--- Desktop/hw1.ml/Hw1.ml
+++ Desktop/hw1.ml/Hw1.ml
@@ -14,29 +14,37 @@ let rec float_pow x n :float=
    else 
    x *. float_pow x (n-1);;
 
-  (*----------Part 1 Q2-------------------*)
+(*----------Part 1 Q2-------------------*)
 let rec compress lst  = 
     match lst with 
     |[] -> []
     |[head] -> [head]
     |head :: head_of_tail::tail -> if head = head_of_tail then (compress(head_of_tail::tail)) else head::(compress(head_of_tail::tail));;
 
- (*------------------Part 1 Q3---------------------*)  
+(*------------------Part 1 Q3---------------------*)  
 let rec remove_if lst p = 
   match lst with 
   |[] -> []
   |head::tail -> if p head = true then remove_if tail p else head::(remove_if tail p);;
 
  
- (*---------------------Part 1 Q4----------------------*)
+(*---------------------Part 1 Q4----------------------*)
+
 
 (*---------------------Part 1 Q5-----------------*)
   
 
 
 (*----------------Part 1 Q6------------------------*)
+let check_if_prime n :bool = 
+  if (n<=2 || n mod 2 = 0) then false else true 
 
-
+let goldbachpair num  = 
+  let rec helper i = 
+    if (check_if_prime i = true && (check_if_prime (num-i)) = true) then (i, num-i)
+    else 
+    helper(i + 1) 
+    in helper 0 ;;
 (*---------------------Part 1 Q7-----------------------*)
 let rec equiv_on f g lst : bool =
        match lst with 
@@ -47,15 +55,19 @@ let rec equiv_on f g lst : bool =
        false ;; 
 
 (*-----------------Part 1 Q8-----------------*)
-(*let rec pairwisefilter cmp lst  = 
+let rec pairwisefilter cmp lst  = 
           match lst with 
           | [] -> []
           |[head] -> [head]
-          |head::head_of_tail::tail -> pairwisefilter((cmp(head head_of_tail)) tail);;*)
+          |head::head_of_tail::tail -> if (cmp head head_of_tail) = head then head :: (pairwisefilter cmp tail) 
+          else head_of_tail :: (pairwisefilter cmp tail );;
 
 
-        (* ---------------Part 1 Q9----------------*)
+(* ---------------Part 1 Q9----------------*)
 let  rec polynomial lst n = 
             match lst with 
             |[] -> 0
             |(num,exp)::tail -> (num * (pow n exp)) + polynomial tail n ;;
+
+(*-------- Part 1 Q10------------------*)
+        
