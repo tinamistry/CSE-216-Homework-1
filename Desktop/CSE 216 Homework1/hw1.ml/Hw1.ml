b28b807ab41aedb3a99448e39092b90974326f5f
Tina Mistry <tmistry10@gmail.com>
2021-10-05 18:29:02 -0400

    Adding Hw1 Q 2.4

--- Desktop/CSE 216 Homework1/hw1.ml/Hw1.ml
+++ Desktop/CSE 216 Homework1/hw1.ml/Hw1.ml
@@ -20,6 +20,7 @@ let rec compress lst  =
     |[] -> []
     |[head] -> [head]
     |head :: head_of_tail::tail -> if head = head_of_tail then (compress(head_of_tail::tail)) else head::(compress(head_of_tail::tail));;
+  
 
 (*------------------Part 1 Q3---------------------*)  
 let rec remove_if lst p = 
@@ -29,15 +30,46 @@ let rec remove_if lst p =
 
  
 (*---------------------Part 1 Q4----------------------*)
+let rec slice_j lst j  = 
+  match lst with 
+  |[] -> []
+  |head :: tail -> if j = 0 then [] else  head :: slice_j tail (j-1);;
 
+let rec slice_i lst i = 
+  match lst with 
+  |[] -> []
+  |head :: tail -> if i = 0 then lst else slice_i tail (i-1)
+
+let slice lst i j = 
+  match lst with 
+  |[] -> []
+  |head :: tail -> if i > j then [] else slice_i (slice_j lst j ) i ;;
+  
 
 (*---------------------Part 1 Q5-----------------*)
+(*let rec equivs_helper1 f lst = 
+  match lst with 
+  |[] -> []
+  |head::tail -> if (f head)then (head :: equivs_helper1(f tail)) else ((equivs_helper1(f tail) );; 
   
+  let rec equivs_helper2 f lst = 
+    match lst with 
+    |[]-> []
+    |head::tail -> if !(f head) then (head :: equivs_helper(f tail)) else (equivs_helper(f tail))
+
+let equivs f lst = 
+  match lst with 
+  |[] -> [[]]
+  |h::t  ->  equivs_helper1(f lst) :: equivs_helper2(f lst)*)
+
+
+
 
 
 (*----------------Part 1 Q6------------------------*)
 let check_if_prime n :bool = 
   if (n<=2 || n mod 2 = 0) then false else true 
+  ;;
 
 let goldbachpair num  = 
   let rec helper i = 
@@ -55,7 +87,7 @@ let rec equiv_on f g lst : bool =
        false ;; 
 
 (*-----------------Part 1 Q8-----------------*)
-let rec pairwisefilter cmp lst  = 
+let rec pairwisefilter cmp lst  = 2
           match lst with 
           | [] -> []
           |[head] -> [head]
@@ -70,4 +102,19 @@ let  rec polynomial lst n =
             |(num,exp)::tail -> (num * (pow n exp)) + polynomial tail n ;;
 
 (*-------- Part 1 Q10------------------*)
-        
+
+let rec powerset lst : 'a list = 
+  match lst with 
+  |[] -> []
+  |head::tail -> [head] :: (powerset tail);;
+
+let rec powerset_helper lst : 'a list list  = 
+  match lst with 
+  |[[]] -> [[]]
+  |(_::_)::[]->[]
+  |[]::[] -> []
+  |head::next::tail -> [head@next] :: (powerset_helper (head::tail));;
+
+
+
+
